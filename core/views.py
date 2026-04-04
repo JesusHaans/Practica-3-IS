2a74ddc8eab82926c937b44b6b19ddd6b1f196a5
JesusHaans <jesuslopez@ciencias.unam.mx>
2026-04-04 14:39:31 -0600

    Feat (core, pedidos):  Iniciando CRUD del sistema empezando por READ listando los pedidos

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Pedido
 
 # Create your views here.
 def home(request):
@@ -21,3 +22,15 @@ def register_view(request):
 @login_required
 def dashboard(request):
     return render(request, "dashboard.html")
+
+@login_required
+def lista_pedidos(request):
+    pedidos = Pedido.objects.filter(usuario=request.user).order_by("-fecha_creacion")
+    context = {"pedidos":pedidos}
+    return render(request, "core/lista_pedidos.html", context)
+
+@login_required
+def  detalle_pedido(request, pedido_id):
+    pedido  = get_object_or_404(Pedido, id = pedido_id, usuario = request.user)
+    context = {"pedido":pedido}
+    return render(request, "core/detalle_pedido.html",context)
